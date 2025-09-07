291639ae89309fbaee2b068ad168e4d1615bd695
five <2974796278@@qq.com>
2025-09-07 17:07:57 +0800

    fix:资源文件

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -17,27 +17,42 @@ pub struct GuashuData {
 }
 #[tauri::command]
 fn json() -> Vec<Guashu> {
-    let current_dir = env::current_dir();
-    let current_path = current_dir.unwrap().join("assets\\data.json");
+    // let current_dir = env::current_dir();
+    // let current_path = current_dir.unwrap().join("assets\\data.json");
 
-    let json_data = fs::read_to_string(current_path).unwrap().to_string();
+    // let json_data = fs::read_to_string(current_path).unwrap().to_string();
 
+    // // 解析JSON数据
+    // let guashu_data: GuashuData =
+    //     serde_json::from_str(&json_data).expect("JSON was not well-formatted");
+
+    // // println!("{:?}", guashu_data);
+
+    // // for guashu in guashu_data.data {
+    // //     println!("ID: {}", guashu.id);
+    // //     println!("卦名: {}", guashu.gua_name);
+    // //     println!("上下: {}", guashu.up_down);
+    // //     println!("卦辞: {}", guashu.gua_ci);
+    // //     println!("爻辞:");
+    // //     for yao in guashu.yao_ci {
+    // //         println!("  {}", yao);
+    // //     }
+    // // }
+    // guashu_data.data
+
+
+  let current_dir = env::current_dir()
+        .expect("无法获取当前目录");
+    
+    let current_path = current_dir.join("assets\\data.json");
+    
+    let json_data = fs::read_to_string(&current_path)
+        .expect(&format!("无法读取文件 {}", current_path.display()));
+    
     // 解析JSON数据
-    let guashu_data: GuashuData =
-        serde_json::from_str(&json_data).expect("JSON was not well-formatted");
-
-    // println!("{:?}", guashu_data);
-
-    // for guashu in guashu_data.data {
-    //     println!("ID: {}", guashu.id);
-    //     println!("卦名: {}", guashu.gua_name);
-    //     println!("上下: {}", guashu.up_down);
-    //     println!("卦辞: {}", guashu.gua_ci);
-    //     println!("爻辞:");
-    //     for yao in guashu.yao_ci {
-    //         println!("  {}", yao);
-    //     }
-    // }
+    let guashu_data: GuashuData = serde_json::from_str(&json_data)
+        .expect("JSON解析失败");
+    
     guashu_data.data
 }
 
